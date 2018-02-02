6e6d15c530c61a87ee0dc3bbb1a17cb3ac8a770e
Linda Foo <linda.foo.dev@gmail.com>
2018-02-02 12:17:47 -0800

    connected client and server

--- src/components/cheese-list.js
+++ src/components/cheese-list.js
@@ -14,6 +14,7 @@ class CheeseList extends React.Component{
       {cheese}
     </li>
   ));
+  console.log('what is cheeses - client - CheeseList render', cheeses);
 
   return (
     <ul id="cheeseList">
